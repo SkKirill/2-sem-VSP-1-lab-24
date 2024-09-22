92470c29f69aa234ad9f71f11c50821b62043335
Kirill <128813627+SkKirill@users.noreply.github.com>
2024-09-22 21:37:59 +0300

    Add Files Lab

---
+++ Solution/Unit1.dfm
@@ -0,0 +1,48 @@
+object Form1: TForm1
+  Left = 134
+  Top = 136
+  Width = 359
+  Height = 130
+  Caption = 'Form1'
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  PixelsPerInch = 96
+  TextHeight = 13
+  object lbl_output: TLabel
+    Left = 24
+    Top = 56
+    Width = 43
+    Height = 13
+    Caption = 'Answer: '
+  end
+  object edt_input: TEdit
+    Left = 24
+    Top = 24
+    Width = 217
+    Height = 21
+    TabOrder = 0
+  end
+  object btn_action: TButton
+    Left = 248
+    Top = 24
+    Width = 75
+    Height = 25
+    Caption = 'Input'
+    TabOrder = 1
+    OnClick = btn_actionClick
+  end
+  object btn_Clear: TButton
+    Left = 248
+    Top = 56
+    Width = 75
+    Height = 25
+    Caption = 'Clear'
+    TabOrder = 2
+    OnClick = ClickClear
+  end
+end
---
+++ Solution/Unit1.pas
@@ -0,0 +1,69 @@
+unit Unit1;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    edt_input: TEdit;
+    lbl_output: TLabel;
+    btn_action: TButton;
+    btn_Clear: TButton;
+    procedure ClickClear(Sender: TObject);
+    procedure btn_actionClick(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.ClickClear(Sender: TObject);
+begin
+  edt_input.Clear;
+  lbl_output.Caption := 'Answer: ';
+end;
+
+procedure TForm1.btn_actionClick(Sender: TObject);
+var
+  i, count: Integer;
+  words, word, UpperWords: string;
+  SetForWord : set of Char;
+begin
+  words := edt_input.Text + ' ';
+  UpperWords := AnsiUpperCase(words);
+  word := '';
+  count := 0;
+  SetForWord := [];
+  for i := 1 to Length(words) do
+    begin
+      if words[i] = ' ' then
+        begin
+          if (word <> '') and (count > 2)
+            then lbl_output.Caption := lbl_output.Caption + word + ' ';
+          count := 0;
+          word := '';
+          SetForWord := [];
+        end
+      else
+        begin
+          if not (UpperWords[i] in SetForWord) then
+            begin
+              SetForWord := SetForWord + [UpperWords[i]];
+              Inc(count);
+            end;
+          word := word + words[i];
+        end;
+    end;
+end;
+
+end.
